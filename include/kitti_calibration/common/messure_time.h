135cc8e0d36153f477cec517d44b4dd8b9d4fd4c
Florian Nolden <fn187-github@yahoo.de>
2015-02-22 18:37:38 +0100

    added function to diff clock_t and returns a string

--- include/kitti_calibration/common/messure_time.h
+++ include/kitti_calibration/common/messure_time.h
@@ -8,9 +8,10 @@
 #ifndef INCLUDE_KITTI_CALIBRATION_COMMON_MESSURE_TIME_H_
 #define INCLUDE_KITTI_CALIBRATION_COMMON_MESSURE_TIME_H_
 
+const std::string spacer = "\t";
+
 std::string time_string(clock_t start, clock_t end, std::string name, int iterations, int size1, int size2, int score=0, bool description = false){
 	std::stringstream ss;
-	std::string spacer = " \t";
 	if(description){
 		ss << "name" << spacer;
 		ss << "start" << spacer << "stop" << spacer;
@@ -30,6 +31,12 @@ std::string time_string(clock_t start, clock_t end, std::string name, int iterat
 	ss << score;
 	ss << std::endl;
 
+	return ss.str();
+}
+
+std::string time_diff(clock_t start, clock_t end){
+	std::stringstream ss;
+	ss << ((double)end - start)/CLOCKS_PER_SEC;
 	return ss.str();
 }
 
